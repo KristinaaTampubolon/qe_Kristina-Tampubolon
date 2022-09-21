70267a88c351dc4d69597d6163ddac187c46c65f
ktampubolon.fintax@gmail.com <kristinatampubolon45@gmail.com>
2022-09-21 22:37:10 +0700

    menyicil ketertinggalan praktikum

---
+++ 12_Object Oriented Programming/praktikum/Challenge2/Kubus.java
@@ -0,0 +1,20 @@
+package Challenge2;
+
+public class Kubus {
+    private int sisi;
+
+    //Volume
+    public int getVolume(int sisi){
+        return sisi*sisi*sisi;
+    }
+
+    //Setter
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+
+    //Getter
+    public int getSisi() {
+        return sisi;
+    }
+}
